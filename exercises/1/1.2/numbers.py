747d98bc1d803c6a47777946e4322e183adb7c3a
Matthew Brown <matt.d.brown.7@gmail.com>
2024-10-06 16:50:31 -0600

    getting numbers beginners example working

---
+++ exercises/1/1.2/numbers.py
@@ -0,0 +1,17 @@
+
+
+print(3+4*5)
+# scientific notation
+print(23.45 / 1e-02)
+
+# integer division
+print(7 / 4)
+print(7 // 4)
+
+x = 1172.5
+print(x.as_integer_ratio())
+print(x.is_integer())
+y = 12345
+print(y.numerator)
+print(y.denominator)
+print(y.bit_length())
